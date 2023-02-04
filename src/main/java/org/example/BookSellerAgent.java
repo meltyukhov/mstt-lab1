734c2efa54d90bf1d24ffa75cc6ff74a686f7d9b
Bohdan Meltiukhov <bmeltiuk@cisco.com>
2023-02-04 20:21:01 +0200

    Initial commit

---
+++ src/main/java/org/example/BookSellerAgent.java
@@ -0,0 +1,126 @@
+package org.example;
+
+import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.OneShotBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+
+import java.util.Hashtable;
+
+public class BookSellerAgent extends Agent {
+    private static final long serialVersionUID = 1L;
+
+    private Hashtable<String, Integer> catalogue;
+    private BookSellerGui myGui;
+
+    protected void setup() {
+        catalogue = new Hashtable<>();
+
+        myGui = new BookSellerGui(this);
+        myGui.showGui();
+
+        DFAgentDescription dfd = new DFAgentDescription();
+        dfd.setName(getAID());
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType("book-selling");
+        sd.setName("JADE-book-trading");
+        dfd.addServices(sd);
+        try {
+            DFService.register(this, dfd);
+        }
+        catch (FIPAException fe) {
+            fe.printStackTrace();
+        }
+
+        addBehaviour(new OfferRequestsServer());
+        addBehaviour(new PurchaseOrdersServer());
+    }
+
+    protected void takeDown() {
+        try {
+            DFService.deregister(this);
+        }
+        catch (FIPAException fe) {
+            fe.printStackTrace();
+        }
+
+        myGui.dispose();
+
+        System.out.println("Seller-agent "+getAID().getName()+" terminating.");
+    }
+
+    public void updateCatalogue(final String title, final int price) {
+        addBehaviour(new OneShotBehaviour() {
+            /**
+             *
+             */
+            private static final long serialVersionUID = 1L;
+
+            public void action() {
+                catalogue.put(title, new Integer(price));
+                System.out.println(title + " inserted into catalogue. Price = " + price);
+            }
+        } );
+    }
+
+    private class OfferRequestsServer extends CyclicBehaviour {
+        private static final long serialVersionUID = 1L;
+
+        public void action() {
+            MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.CFP);
+            ACLMessage msg = myAgent.receive(mt);
+            if (msg != null) {
+                String title = msg.getContent();
+                ACLMessage reply = msg.createReply();
+
+                Integer price = catalogue.get(title);
+                if (price != null) {
+                    reply.setPerformative(ACLMessage.PROPOSE);
+                    reply.setContent(String.valueOf(price.intValue()));
+                }
+                else {
+                    reply.setPerformative(ACLMessage.REFUSE);
+                    reply.setContent("not-available");
+                }
+                myAgent.send(reply);
+            }
+            else {
+                block();
+            }
+        }
+    }
+
+
+    private class PurchaseOrdersServer extends CyclicBehaviour {
+        private static final long serialVersionUID = 1L;
+
+        public void action() {
+            MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.ACCEPT_PROPOSAL);
+            ACLMessage msg = myAgent.receive(mt);
+            if (msg != null) {
+
+                String title = msg.getContent();
+                ACLMessage reply = msg.createReply();
+
+                Integer price = catalogue.remove(title);
+                if (price != null) {
+                    reply.setPerformative(ACLMessage.INFORM);
+                    System.out.println(title+" sold to agent "+msg.getSender().getName());
+                }
+                else {
+                    reply.setPerformative(ACLMessage.FAILURE);
+                    reply.setContent("not-available");
+                }
+                myAgent.send(reply);
+            }
+            else {
+                block();
+            }
+        }
+    }
+}
